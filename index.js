38e0a027640ea24a3d6edfe70d8be7838904be78
Ringo <ringong95@gmail.com>
2017-01-17 15:50:14 -0800

    Redux Logger: Working Middle ware

--- index.js
+++ index.js
@@ -2,44 +2,26 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducer as todoReducer, updateTodoName } from './redux';
 
+// function shoutLogger(theStore) {
+//   return function (next) {
+//     return function dispatchAndLog(action) {
+
+//     };
+//   };
+// }
+
+const logger = theStore => next => (action) => {
+  console.log('DISPATCHING!!', action);
+  const result = next(action);
+  console.log('NEXT STATE!', theStore.getState());
+  return result;
+};
+
 const store = createStore( // eslint-disable-line
   todoReducer,
   composeWithDevTools(
-    applyMiddleware(),
+    applyMiddleware(logger),
   ),
 );
-
-// console.log('dispatching', action);
-// const result = next(action);
-// console.log('next state', store.getState());
-// return result;
-
-function whisperLogger(theStore) {
-  const next = theStore.dispatch;
-  return function dispatchAndLog(action) {
-    console.log('*dispatching*', action);
-    const result = next(action);
-    console.log('*next state*', store.getState());
-    return result;
-  };
-}
-
-function shoutLogger(theStore) {
-  const next = theStore.dispatch;
-  return function dispatchAndLog(action) {
-    console.log('DISPATCHING!!', action);
-    const result = next(action);
-    console.log('NEXT STATE!', store.getState());
-    return result;
-  };
-}
-
-function applyMonkeWare(theStore, monkeywares) { //eslint-disable-line
-  monkeywares.forEach((monkeyware) => {
-    theStore.dispatch = monkeyware(theStore); // eslint-disable-line
-  });
-}
-
-applyMonkeWare(store, [shoutLogger, whisperLogger]);
 
 store.dispatch(updateTodoName('getting there you fuck'));
